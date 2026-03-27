ab62d51d8a68e87f8f7af8018095dbfb3d5b990c
Your Name <you@example.com>
2026-03-27 20:12:00 +0000

    new try

--- list.c
+++ list.c
@@ -48,11 +48,9 @@ void * firstList(List * list)
 {   
     Node* primero = list->head;
 
-    //list->current = primero;
+    list->current = primero;
 
-    //void* dato = list->current->data;
-
-    return primero;
+    return list->current;
 
 }
 
